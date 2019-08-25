e613bad71efba8efa4f72b4802226edeb699210c
pgilfillan <pgilfillan2@gmail.com>
2019-08-25 20:35:00 +1000

    Added enemysprites

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -9,6 +9,8 @@ public class PlayerControl : MonoBehaviour
     public Sprite archerSprite;
     public Sprite mageSprite;
 
+    private ClassState currClassState = ClassState.Warrior;
+
     void Update()
     {
         float horizontal = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
@@ -18,14 +20,54 @@ public class PlayerControl : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
             GetComponent<SpriteRenderer>().sprite = warriorSprite;
+            currClassState = ClassState.Warrior;
         }
         else if (Input.GetKeyDown(KeyCode.Alpha2))
         {
             GetComponent<SpriteRenderer>().sprite = archerSprite;
+            currClassState = ClassState.Archer;
         }
         else if (Input.GetKeyDown(KeyCode.Alpha3))
         {
             GetComponent<SpriteRenderer>().sprite = mageSprite;
+            currClassState = ClassState.Mage;
         }
+        else if (Input.GetMouseButtonDown(0))
+        {
+            switch (currClassState)
+            {
+                case ClassState.Warrior:
+                    Debug.Log("Attacked with warrior primary");
+                    break;
+                case ClassState.Archer:
+                    Debug.Log("Attacked with archer primary");
+                    break;
+                case ClassState.Mage:
+                    Debug.Log("Attacked with mage primary");
+                    break;
+            }
+        }
+        else if (Input.GetMouseButtonDown(1))
+        {
+            switch (currClassState)
+            {
+                case ClassState.Warrior:
+                    Debug.Log("Attacked with warrior secondary");
+                    break;
+                case ClassState.Archer:
+                    Debug.Log("Attacked with archer secondary");
+                    break;
+                case ClassState.Mage:
+                    Debug.Log("Attacked with mage secondary");
+                    break;
+            }
+        }
+    }
+
+    private enum ClassState
+    {
+        Warrior,
+        Archer,
+        Mage
     }
 }
