d031f17dc957689bda5a5e561b29921830ec71ce
pgilfillan <pgilfillan2@gmail.com>
2019-08-25 15:18:46 +1000

    Sprites and basic movement

--- Assets/GUI/Scripts/MainMenu.cs
+++ Assets/GUI/Scripts/MainMenu.cs
@@ -1,12 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
 	public void OnPlayButtonClicked()
     {
-    	Application.LoadLevel("CombatTest");
+        SceneManager.LoadScene("CombatTest");
     }
 
 	public void OnOptionButtonClicked()
