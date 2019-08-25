5c9dd434aee9ce1bdd62074fcca0cc62ad00ad98
pgilfillan <pgilfillan2@gmail.com>
2019-08-25 15:36:17 +1000

    Switching between sprites

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float speed;
+    public Sprite warriorSprite;
+    public Sprite archerSprite;
+    public Sprite mageSprite;
+
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        float vertical = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        transform.Translate(horizontal, vertical, 0);
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            GetComponent<SpriteRenderer>().sprite = warriorSprite;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            GetComponent<SpriteRenderer>().sprite = archerSprite;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            GetComponent<SpriteRenderer>().sprite = mageSprite;
+        }
+    }
+}
