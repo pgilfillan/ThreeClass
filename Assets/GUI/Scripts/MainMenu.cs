8f808683c9d26ffde7a42cdf7ccc9a50551788d7
pgilfillan <pgilfillan2@gmail.com>
2019-08-25 13:30:29 +1000

    Simple main menu

---
+++ Assets/GUI/Scripts/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+	public void OnPlayButtonClicked()
+    {
+    	Application.LoadLevel("CombatTest");
+    }
+
+	public void OnOptionButtonClicked()
+    {
+    	Debug.Log("Load options menu");
+    }
+
+    public void OnQuitButtonClicked()
+    {
+    	Application.Quit();
+    }
+}
